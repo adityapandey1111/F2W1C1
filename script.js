6639051af3ecbaff5736a927d682ad4153916155
Aditya Kumar Pandey <114282369+adityapandey1111@users.noreply.github.com>
2023-05-13 20:31:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,35 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+   console.log(arr.filter(arr => arr.marks > 50).map(arr => arr.name));
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(arr => {
+    if (arr.marks > 50) console.log(arr.name);
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  const newStudent = {id:arr.length+1, name:"susan", age:"20", marks:45};
+  arr.push(newStudent);
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  arr = arr.filter(arr => arr.marks >= 50);
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  arr.push(
+    {id:arr.length+1, name:"samantha", age:"21", marks:90},
+    {id:arr.length+2, name:"kevin", age:"19", marks:70},
+    {id:arr.length+3, name:"emily", age:"20", marks:55}
+  );
+  console.log(arr);
 }
